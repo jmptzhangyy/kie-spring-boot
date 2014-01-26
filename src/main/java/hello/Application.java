30b637ca58aeca1e43d51daf7ebfebb50e259ec6
vinod <vinodkiran@usa.net>
2014-01-26 23:17:24 +0530

    removing debug code/classes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,12 @@
 package hello;
 
-import java.net.MalformedURLException;
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.Resource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAutoConfiguration
